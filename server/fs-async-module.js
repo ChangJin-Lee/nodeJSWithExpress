7f4202fd14e0d9c4898e95a9662fd6ea1813bbb1
ChangJin-Lee <ckdwls525@gmail.com>
2023-08-10 00:29:47 +0900

    update file system async module
    update file system async module

---
+++ server/fs-async-module.js
@@ -0,0 +1,30 @@
+const {readFile, writeFile} = require('fs'); 
+
+// 콜백을 제공할거에요~
+// utf8 안써주면 버퍼가 나와여 
+// <Buffer 68 65 6c 6c 6f 20 74 68 69 73 20 69 73 20 66 69 72 73 74 20 66 69 6c 65>
+readFile('./content/first.txt', 'utf8', (err, result) => {
+    if (err){
+        console.log(err)
+        return 
+    }
+    const first = result
+    console.log(result)
+    readFile('./content/second.txt','utf8', (err, result)=>{
+        if (err){
+            console.log(err)
+            return 
+        }
+        const second = result
+        writeFile(
+        './content/result-async.txt',
+        `Here is the result ${first} \n${second}`,
+         (err,result) =>{
+            if(err){
+                console.log(err);
+                return
+            }
+            console.log(result);
+        })
+    })
+})

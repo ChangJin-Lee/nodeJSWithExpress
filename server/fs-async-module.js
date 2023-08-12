b93f4a194f19ec4b09c11e24fd92854bd41deb1f
ChangJin-Lee <ckdwls525@gmail.com>
2023-08-13 01:06:38 +0900

    update file system module

--- server/fs-async-module.js
+++ server/fs-async-module.js
@@ -3,6 +3,8 @@ const {readFile, writeFile} = require('fs');
 // 콜백을 제공할거에요~
 // utf8 안써주면 버퍼가 나와여 
 // <Buffer 68 65 6c 6c 6f 20 74 68 69 73 20 69 73 20 66 69 72 73 74 20 66 69 6c 65>
+// 순간에 이미 offload됨.
+console.log('start');
 readFile('./content/first.txt', 'utf8', (err, result) => {
     if (err){
         console.log(err)
@@ -25,6 +27,9 @@ readFile('./content/first.txt', 'utf8', (err, result) => {
                 return
             }
             console.log(result);
+            console.log('done with this task');
         })
     })
 })
+
+console.log('starting next task');
--- server/fs-sync-module.js
+++ server/fs-sync-module.js
@@ -1,10 +1,16 @@
 const {readFileSync, writeFileSync} = require('fs'); 
 const fs = require('fs'); 
-
+console.log('start');
 const first = fs.readFileSync('./content/first.txt', 'utf8');
 const second = readFileSync('./content/second.txt', 'utf8');
 
 console.log(first, second);
 
 // flag는 우리가 알고 있는 이어붙이기 새로 쓰기  그거임.
-writeFileSync('./content/first-write.txt', `Here is the result ${first} \n${second}`, { flag: 'a'})
+writeFileSync('./content/first-write.txt',
+ `Here is the result ${first} \n${second}`,
+  { flag: 'a'}
+  )
+
+console.log('done with this task');
+console.log('starting the next one');
